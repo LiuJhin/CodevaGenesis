c7ccb5ffd63ca99d0bdd47c09b7b11afc8dcb5d8
LiuJhin <1197327429@qq.com>
2024-07-27 09:36:02 +0800

    feat：新增project页

--- pages/project.vue
+++ pages/project.vue
@@ -1,20 +1,33 @@
 <template>
-  <div>
-    <div></div>
-    <div>
-      <div></div>
-      <div></div>
+  <div class="project-page">
+    <div class="irregular-content">
+      <div class="irregular-content-inner">开发中</div>
     </div>
-    <div></div>
-    <div>
-      <div></div>
-    </div>
-    <div></div>
   </div>
 </template>
 
-<script>
-export default {};
-</script>
+<script setup></script>
 
-<style></style>
+<style scoped lang="scss">
+.project-page {
+  width: 100%;
+  height: 100vh;
+
+  .irregular-content {
+    width: 100%;
+
+    .irregular-content-inner {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      height: 400px;
+    }
+
+    .irregular-content {
+      width: 100%;
+      height: 400px;
+    }
+  }
+}
+</style>
