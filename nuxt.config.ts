da46848346db822151693cf0eb50ec46ded84434
LiuJhin <1197327429@qq.com>
2024-08-15 22:51:31 +0800

    fix: 新增i18n多语言配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,13 +7,16 @@ const { resolve } = createResolver(import.meta.url);
 export default defineNuxtConfig({
   ssr: false,
   css: ["~/assets/style.scss"],
-  modules: ['@pinia/nuxt',
-      '@nuxtjs/tailwindcss',
-      '@nuxtjs/i18n'
-    ],
-    build: {
-        transpile: ['gsap'],
-    },
+  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+  build: {
+    transpile: ["gsap"],
+  },
+  i18n: {
+    strategy: "prefix_and_default", // 添加路由前缀的方式
+    locales: ["en", "zh"], //配置语种
+    defaultLocale: "en", // 默认语种
+    vueI18n: "./i18n.config.ts", // 通过vueI18n配置
+  },
   devtools: { enabled: true },
   compatibilityDate: "2024-07-23",
-})
+});
